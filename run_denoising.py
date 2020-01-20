792d99e70edbc01225b0f653e61a53b80ca82dd4
smikhai1 <sidorenko.mikhail94@gmail.com>
2020-01-20 13:08:46 +0300

    first working version

---
+++ run_denoising.py
@@ -0,0 +1,71 @@
+from argparse import ArgumentParser
+import torch
+from tqdm import tqdm
+import os
+import cv2
+import numpy as np
+import torch.nn as nn
+from torch.utils.tensorboard import SummaryWriter
+
+from src.models.unet import UNet
+
+
+def parse_args():
+
+    parser = ArgumentParser()
+
+    parser.add_argument("--num_iter", type=int, help="Number of optimization steps")
+    parser.add_argument("--save_step", type=int, help="Step for saving results to TB")
+    parser.add_argument("--lr", type=float, help="Learning rate")
+    parser.add_argument("--result_dir", type=str, help="Path to the dir with the result")
+    parser.add_argument("--log_dir", type=str, help="Path to dir with logs")
+    parser.add_argument("--image_path", type=str, help="Path to the image file")
+    parser.add_argument("--image_size", type=int, help="Size of the image")
+
+    args = parser.parse_args()
+
+    return args
+
+def load_image(image_path, image_size):
+    pass
+
+def main():
+
+    args = parse_args()
+    writer = SummaryWriter(args.log_dir)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f"[*] Active device: {device}")
+    model = UNet(32, [128]*5, [128]*5, [4]*4, [3]*5, [3]*5, [1]*4, "bilinear")
+    model.to(device)
+
+    input_ = torch.rand([1, 32, 256, 256]) * 0.1
+    img = cv2.imread(args.image_path, cv2.IMREAD_GRAYSCALE) / 255.0
+    img = img[100:args.image_size+100, 100:args.image_size+100]
+    if img.ndim == 2:
+        img = np.expand_dims(img, axis=0)
+    img = np.expand_dims(img, axis=0)
+    img = torch.from_numpy(img)
+    img = img.to(torch.float32)
+
+    img = img.to(device)
+    input_ = input_.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    loss = nn.MSELoss()
+
+    print("[*] Optimization started")
+    for i in tqdm(range(1, args.num_iter+1)):
+        optimizer.zero_grad()
+        out = model(input_)
+        loss_val = loss(out, img)
+        loss_val.backward()
+        optimizer.step()
+
+        if i % args.save_step == 0:
+            out_ = out.detach().cpu()
+            out_ = out_[0]
+            writer.add_image("result", out_, i)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/models/unet.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+
+from copy import deepcopy
+
+from src.models import get_downsample_layer, get_skip_layer, get_upsample_layer
+
+class UNet(nn.Module):
+
+    def __init__(self, num_ch, n_d, n_u, n_s, k_d, k_u, k_s, interpolation):
+        """
+        UNet-based model for denoising with skip-connections
+
+        :param num_ch: int, number of channels in the fixed input
+        :param n_d: list, numbers of filters in the downsampling layer
+        :param n_u: list, numbers of filters in the upsampling layer
+        :param n_s: list, numbers of filters in the skip-connection layer
+        :param k_d: list, kernel sizes for the downsampling layer
+        :param k_u: list, kernel sizes for the upsampling layer
+        :param k_s: list, kernel sizes for the skip-connection layer
+        :param interpolation: str, upsampling mode
+        """
+        super().__init__()
+
+        self.encoder = []
+        self.encoder.append(get_downsample_layer(in_channels=num_ch,
+                                                 num_filters=n_d[0],
+                                                 kernel_size=k_d[0]
+                                                 )
+                            )
+        for i in range(1, len(n_d)):
+            self.encoder.append(get_downsample_layer(in_channels=n_d[i-1],
+                                                     num_filters=n_d[i],
+                                                     kernel_size=k_d[i]
+                                                     )
+                                )
+
+        self.decoder = []
+        self.decoder.append(get_upsample_layer(in_channels=n_d[-1],
+                                               num_filters=n_u[0],
+                                               kernel_size=k_u[0],
+                                               interpolation=interpolation
+                                               )
+                            )
+        for i in range(1, len(n_u)):
+            self.decoder.append(get_upsample_layer(in_channels=n_u[i-1]+n_s[-i],
+                                                   num_filters=n_u[i],
+                                                   kernel_size=k_u[i],
+                                                   interpolation=interpolation
+                                                  )
+                                )
+        end = nn.Sequential(nn.Conv2d(in_channels=n_u[-1], out_channels=1, kernel_size=1),
+                            nn.Sigmoid()
+                            )
+        self.decoder.append(end)
+
+        self.skipper = [get_skip_layer(in_channels=n_d[i], num_filters=n_s[i], kernel_size=k_s[i])
+                        for i in range(len(n_s))
+                        ]
+
+    def forward(self, x):
+
+        out = deepcopy(x)
+
+        encoder_outs = []
+        for i in range(len(self.encoder)):
+            out = self.encoder[i].forward(out)
+            encoder_outs.append(out)
+
+        out = self.decoder[0].forward(out)
+        for i in range(1, len(self.decoder)-1):
+            skip = self.skipper[-i].forward(encoder_outs[-i-1])
+            out = self.decoder[i].forward(torch.cat((out, skip), dim=1))
+
+        out = self.decoder[-1].forward(out)
+
+        return out
+
+    def parameters(self):
+
+        params = []
+        for block in self.encoder:
+            params.extend(list(block.parameters()))
+
+        for block in self.skipper:
+            params.extend(list(block.parameters()))
+
+        for block in self.decoder:
+            params.extend(list(block.parameters()))
+
+        return params
+
+    def to(self, device):
+
+        for block in self.encoder:
+            block.to(device)
+
+        for block in self.skipper:
+            block.to(device)
+
+        for block in self.decoder:
+            block.to(device)
