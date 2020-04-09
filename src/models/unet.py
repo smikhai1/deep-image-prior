7ca21a2b9ef88d67d021285835bc4fcdc616117b
smikhai1 <sidorenko.mikhail94@gmail.com>
2020-04-09 22:03:09 +0300

    model debugged; checks of arguments added

--- src/models/unet.py
+++ src/models/unet.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from typing import Any, List, Union
+from typing import List, Union
 from copy import deepcopy
 
 from src.models.layers import DownsamplingBlock, SkipConnectionBlock, UpsamplingBlock
@@ -17,12 +17,40 @@ class UNet(nn.Module):
                  k_s: Union[List[int], int],
                  upsampling: str = 'nearest'
                  ):
+        """
+        Hourglass-like model for denoising in Deep Image Prior framework.
+
+        :param in_channels: int, number of channels in the input tensor
+        :param out_channels: int, number of channels in the output tensor
+        :param n_filters: list, list that contains the number of filters used in each block
+        :param k_d: int or list, sets the kernel size in each downsampling block; if int is given,
+                    the kernel size will be the same in each block
+        :param k_u: int or list, sets the kernel size in each upsampling block; if int is given,
+                    the kernel size will be the same in each block
+        :param n_skips: list, sets the number of channels in the skip-connection block; 0 corresponds
+                        to the absence of skip-connection
+        :param k_s: list or int, sets the kernel size in each skip-connection block; if int is given,
+                    the kernel size will be the same in each block
+        :param upsampling: str, sets the upsampling mode ['nearest', 'bicubic', 'bilinear']
+        """
         super().__init__()
+        self.num_scales = len(n_filters)
+
+        if isinstance(k_d, int):
+            k_d = [k_d] * self.num_scales
+        if isinstance(k_u, int):
+            k_u = [k_u] * self.num_scales
+        if isinstance(k_s, int):
+            k_s = [k_s] * self.num_scales
+
+        if not self.num_scales == len(k_d) == len(k_u) == len(k_s) == len(n_skips):
+            raise ValueError('There are incorrect arguments in the constructor!')
+
         num_down_channels = deepcopy(n_filters)
         num_down_channels.insert(0, in_channels)
         num_up_channels = deepcopy(n_filters)
         num_up_channels.insert(0, out_channels)
-        self.num_scales = len(n_filters)
+
         for i in range(self.num_scales):
             setattr(self,
                     'down_block_{}'.format(i),
@@ -42,8 +70,10 @@ class UNet(nn.Module):
                     )
 
     def forward(self, x):
+
         out = x
         skips_outs = []
+
         # downsampling and skip-connections paths
         for i in range(self.num_scales):
             out = getattr(self, 'down_block_{}'.format(i))(out)
